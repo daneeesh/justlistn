6fba3aba53a7db982d1c6a9e34553dae7e697a00
daneeesh <denes@addepar.com>
2015-08-12 00:11:47 +0200

    enabled genres too, moved things around a bit

--- startbootstrap-creative-1.0.1/js/creative.js
+++ startbootstrap-creative-1.0.1/js/creative.js
@@ -4,7 +4,7 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
-var sc_query_url = 'https://api.soundcloud.com/tracks?client_id=c7e286fe6e5cdc8424f2bf68c2ff14a7&q=';
+var sc_query_url = 'https://api.soundcloud.com/tracks?client_id=c7e286fe6e5cdc8424f2bf68c2ff14a7&';
 var widget_base_url = 'http://api.soundcloud.com/tracks/';
 var widget_url_extension = '&visual=true&hide_related=true&show_comments=false';
 
@@ -20,6 +20,12 @@ $(document).ready(function() {
         var iframeElement = document.querySelector('iframe');
         var iframeElementID = iframeElement.id;
         widget1 = SC.Widget(iframeElement);
+        widget1.bind(SC.Widget.Events.FINISH, function() {
+            $('#sc-widget').hide();
+            document.getElementById('main_search').value = "";
+            $('#main_search').prop('disabled', false);
+            $('#main_search').show();
+        });
     });
 
     // jQuery for page scrolling feature - requires jQuery Easing plugin
@@ -34,29 +40,41 @@ $(document).ready(function() {
     $('#listn-btn').on('click', function(event) {
         $('#main_search').prop('disabled', true);
         var query = document.getElementById('main_search').value;
-        var url = sc_query_url + query + '&embeddable_by=all&limit=1';
+        var url = sc_query_url + 'q=' + query + '&embeddable_by=all&limit=10';
         var track_id;
         var dfrd1 = $.Deferred();
         $.getJSON(url, function(tracks) {
-            track_id = tracks[0]['id'];
+            var track_number = Math.floor(Math.random() * tracks.length);
+            track_id = tracks[track_number]['id'];
             console.log(track_id);
             widget1.bind(SC.Widget.Events.READY, function() {
                 // show the SC widget when it's ready
                 $('#main_search').hide();
                 $('#sc-widget').show();
             });
-            widget1.bind(SC.Widget.Events.FINISH, function() {
-                $('#sc-widget').hide();
-                document.getElementById('main_search').value = "";
-                $('#main_search').prop('disabled', false);
-                $('#main_search').show();
-            })
             widget1.load(widget_base_url + track_id + widget_url_extension, {auto_play:true});
             dfrd1.resolve();
         });
         //$.when(dfrd1).done(function(){
         //    $('#main_search').hide();
         //});
+    })
+
+    $('.portfolio-box').click(function(event) {
+        $('#main_search').prop('disabled', true);
+        var genre = this.getElementsByClassName('project-name')[0].innerHTML.toLowerCase().trim();
+        var url = sc_query_url + 'genres=' + genre + '&embeddable_by=all&limit=50';
+        var track_id;
+        $.getJSON(url, function(tracks) {
+            var track_number = Math.floor(Math.random() * tracks.length);
+            track_id = tracks[track_number]['id'];
+            widget1.bind(SC.Widget.Events.READY, function() {
+                // show the SC widget when it's ready
+                $('#main_search').hide();
+                $('#sc-widget').show();
+            });
+            widget1.load(widget_base_url + track_id + widget_url_extension, {auto_play:true});
+        });
     })
 
     // Highlight the top nav as scrolling occurs
